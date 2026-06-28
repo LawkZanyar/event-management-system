227f4a94278af055dfae2ff20987e3104da1ef9b
LawkZanyar <lzqiu230018@uniq.edu.qiu>
2026-06-28 12:35:14 +0300

    connect railway postgres

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,14 +1,30 @@
+import os
 
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./events.db"
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgres://",
+        "postgresql://",
+        1
+    )
+
 
 engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    DATABASE_URL
 )
 
-SessionLocal = sessionmaker(bind=engine)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
 
 Base = declarative_base()
