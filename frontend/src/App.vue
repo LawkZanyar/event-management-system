8e17e027f2fcd401b7d4e65d0e98e48de1a1488b
LawkZanyar <lzqiu230018@uniq.edu.qiu>
2026-06-28 11:50:03 +0300

    fix frontend api

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,19 +4,45 @@ import axios from 'axios'
 
 const events = ref([])
 
+const API_URL =
+  "mindful-manifestation-production.up.railway.app/api/events"
+
+
 onMounted(async () => {
-  const res = await axios.get('mindful-manifestation-production.up.railway.app/api/events')
-  events.value = res.data
+
+  try {
+
+    const response = await axios.get(API_URL)
+
+    console.log(response.data)
+
+    events.value = response.data
+
+  } catch(error) {
+
+    console.log(error)
+
+  }
+
 })
+
 </script>
 
+
 <template>
+
 <h1>Event Management System</h1>
 
+
 <ul>
+
 <li v-for="event in events" :key="event.id">
+
 {{ event.title }} - {{ event.location }}
+
 </li>
+
 </ul>
+
 
 </template>
