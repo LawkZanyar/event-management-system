d120da4f012fb9c7ded795c5cff00f43f619a302
LawkZanyar <lzqiu230018@uniq.edu.qiu>
2026-06-28 11:56:25 +0300

    fix delete and cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,28 @@
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.database import Base, engine
 from app.routes import router
 
+
 Base.metadata.create_all(bind=engine)
 
+
 app = FastAPI(
-    title="Event Management REST API",
-    version="1.0"
+    title="Event Management REST API"
 )
 
-app.include_router(router, prefix="/api")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(
+    router,
+    prefix="/api"
+)
