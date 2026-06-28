c0ca363ca83187056c90f5dd1b5bb1608097e4a4
LawkZanyar <lzqiu230018@uniq.edu.qiu>
2026-06-28 11:31:59 +0300

    fix vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,11 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue()
-  ]
+  ],
+  server: {
+    host: true,
+    allowedHosts: [
+      "innovative-truth-production-576f.up.railway.app"
+    ]
+  }
 })
